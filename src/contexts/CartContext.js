90506eb796d3152513d001fd0faf9495c1331a9b
kleber <kleberenriqueos@gmail.com>
2023-08-07 20:46:34 -0500

    avance

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,11 +1,35 @@
 import React,{createContext, useState,useEffect} from 'react';
+import CartItem from '../components/CartItem';
 
 export const CartContext = createContext();
 
 const CartProvider = ({children}) => {
 
   const [cart, setCart] = useState([]);
-  return <CartContext.Provider value={{cart, setCart}} >{children}</CartContext.Provider>;
+
+  const addToCart = (product,id)=>{
+    const newItem = {...product, amount: 1};
+    const cartItem = cart.find((item)=>{
+      return item.id === id;
+    });
+    // console.log(cartItem);
+    if(cartItem){
+      const newCart = [...cart].map(item =>{
+        if(item.id === id){
+          return{...item,amount:cartItem.amount + 1};
+        }else{
+          return item;
+        }
+      });
+      setCart(newCart);
+    }else{
+      setCart([...cart,newItem]);
+    }
+  };
+  console.log(cart);
+
+
+  return <CartContext.Provider value={{addToCart}} >{children}</CartContext.Provider>;
 };
 
 
