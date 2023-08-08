a552858be83c99e0b15cedc0a691263dad8bd7cf
kleber chavez <kleberenriqueos@gmail.com>
2023-08-08 07:38:51 -0500

    actualizacion

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,9 +8,9 @@ import { useContext } from 'react';
 import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
+
   const {isOpen, handleCLose } = useContext(SidebarContext);
-  console.log(useContext(CartContext));
-  // const {cart, setCart} = useContext(CartContext);
+  const {cart, clearCart } = useContext(CartContext);
 
   return (
   <div className={` ${ isOpen ? 'right-0' : '-right-full' } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`} >
@@ -20,7 +20,21 @@ const Sidebar = () => {
       </div>
       {/* iconos */}
       <div onClick={handleCLose} className='cursor-pointer w-8 h-8 flex justify-center items-center'>
-        |<IoMdArrowForward className='text-2xl  '/>
+        |<IoMdArrowForward className='text-2xl'/>
+      </div>
+    </div>
+    <div>{cart.map(item =>{
+      return <CartItem item ={item} key={item.id} />;
+    })}
+    </div>
+    <div className='flex flex-col gap-y-3 py-4 mt-4' >
+      <div className=' flex w-full justify-between items-center' >
+        {/* total */}
+        <div className='uppercase font-semibold' ><span className='mr-2' >Total:</span>$ 1000</div>
+        {/* limpiar */}
+        <div onClick={clearCart} className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl '>
+          <FiTrash2/>
+        </div>
       </div>
     </div>
   </div>
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -8,11 +8,13 @@ const CartProvider = ({children}) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product,id)=>{
+    
     const newItem = {...product, amount: 1};
+    
     const cartItem = cart.find((item)=>{
       return item.id === id;
     });
-    // console.log(cartItem);
+  
     if(cartItem){
       const newCart = [...cart].map(item =>{
         if(item.id === id){
@@ -26,10 +28,49 @@ const CartProvider = ({children}) => {
       setCart([...cart,newItem]);
     }
   };
-  console.log(cart);
 
+  const removeFromCart = (id) =>{
+    const newCart = cart.filter((item) => {
+      return item.id !== id;
+    });
+    setCart(newCart);
+  };
 
-  return <CartContext.Provider value={{addToCart}} >{children}</CartContext.Provider>;
+  const clearCart = () =>{
+    setCart([]);
+  };
+
+  const increaseAmount = (id) =>{
+    const cartItem = cart.find((item) => item.id === id);
+    addToCart(cartItem,id);
+  };
+
+  const decreaseAmount = (id) =>{
+    const cartItem = cart.find((item) =>{
+      return item.id === id;
+    });
+    if (cartItem){
+      const newCart = cart.map((item) =>{
+          if(item.id === id){
+            return{...item,amount:cartItem.amount - 1};
+          }else{
+            return item;
+          }  
+      });
+      setCart(newCart);
+    }else{
+      if(cartItem.amount < 2){
+        removeFromCart(id);
+    }
+  };
+  return (<CartContext.Provider 
+    value={{
+      cart, 
+      addToCart,
+      removeFromCart,
+      clearCart,
+      increaseAmount,
+    decreaseAmount}} >{children}</CartContext.Provider>);
 };
 
 
