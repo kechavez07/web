230fd415e28f3179cec10578ea2802e5a9abf365
kleber chavez <kleberenriqueos@gmail.com>
2023-08-12 23:15:16 -0500

    correccion

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,6 +11,7 @@ const Sidebar = () => {
 
   const {isOpen, handleCLose } = useContext(SidebarContext);
   const {cart, clearCart,total} = useContext(CartContext);
+  console.log(parseFloat(total).toFixed(2));
 
   return (
   <div className={` ${ isOpen ? 'right-0' : '-right-full' } w-full bg-white fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px]`} >
@@ -31,12 +32,14 @@ const Sidebar = () => {
     <div className='flex flex-col gap-y-3 py-4 mt-4' >
       <div className=' flex w-full justify-between items-center' >
         {/* total */}
-        <div className='uppercase font-semibold' ><span className='mr-2' >Total:</span>$ {parseFloat(total)}</div>
+        <div className='uppercase font-semibold' ><span className='mr-2' >Total:</span>$ {parseFloat(total).toFixed(2)}</div>
         {/* limpiar */}
         <div onClick={clearCart} className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex justify-center items-center text-xl '>
           <FiTrash2/>
         </div>
       </div>
+      <Link to='/' className='bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium' > View Cart</Link>
+      <Link to='/' className='bg-primary flex p-4 justify-center items-center text-whitw w-full font-medium' > Checkout</Link>
     </div>
   </div>
   );
